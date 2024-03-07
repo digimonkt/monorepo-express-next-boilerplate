6a937bf06ab6f783d88328974142c1f9ec065f1c
Akash <akkygreen14@gmail.com>
2024-03-07 10:46:43 +0530

    modified Access-Control-Expose-Header

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -35,10 +35,7 @@ app.use(fileUpload());
 
 app.use((_req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization, x-access, x-refresh",
-  );
+  res.setHeader("Access-Control-Expose-Headers", "x-access, x-refresh");
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, PATCH, PUT, DELETE, OPTIONS",
